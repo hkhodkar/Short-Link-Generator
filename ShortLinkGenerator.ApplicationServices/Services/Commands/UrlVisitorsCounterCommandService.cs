996017d18514a3fabafbbde9e972a3fc3639d8f0
hatef <hatef@HATEF-PC>
2020-11-29 15:26:00 +0330

    Implement UrlVisitorsCounter Services

---
+++ ShortLinkGenerator.ApplicationServices/Services/Commands/UrlVisitorsCounterCommandService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using ShortLinkGenerator.ApplicationContracts.Interfaces.Commands;
+using System.Threading.Tasks;
+using ShortLinkGenerator.DomainContracts.Interfaces.Commamds;
+
+namespace ShortLinkGenerator.ApplicationServices.Services.Commands
+{
+    public class UrlVisitorsCounterCommandService : IUrlVisitorsCounterCommandService
+    {
+        private readonly IUrlVisitorsCounterCommandRepository _urlVisitorsCounterCommandRepository;
+
+        public UrlVisitorsCounterCommandService( IUrlVisitorsCounterCommandRepository urlVisitorsCounterCommandRepository)
+        {
+            _urlVisitorsCounterCommandRepository = urlVisitorsCounterCommandRepository;
+        }
+        public async Task AddCount(string linkCode)
+        {
+            await _urlVisitorsCounterCommandRepository.AddCounter(linkCode);
+        }
+    }
+}
---
+++ ShortLinkGenerator.ApplicationServices/Services/Queries/UrlVisitorsCounterQueryService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ShortLinkGenerator.ApplicationContracts.Interfaces.Queries;
+using ShortLinkGenerator.DomainContracts.Interfaces.Queries;
+
+namespace ShortLinkGenerator.ApplicationServices.Services.Queries
+{
+    public class UrlVisitorsCounterQueryService : IUrlVisitorsCounterQueryService
+    {
+        private IUrlVisitorsCounterQueryRepository _urlVisitorsCounterQueryRepository;
+
+        public UrlVisitorsCounterQueryService(IUrlVisitorsCounterQueryRepository urlVisitorsCounterQueryRepository)
+        {
+            _urlVisitorsCounterQueryRepository = urlVisitorsCounterQueryRepository;
+        }
+
+        public async Task<long> GetUrlVisitorsCount(string linkCode)
+        {
+            return await _urlVisitorsCounterQueryRepository.GetUrlVisitorsCount(linkCode);
+        }
+    }
+}
