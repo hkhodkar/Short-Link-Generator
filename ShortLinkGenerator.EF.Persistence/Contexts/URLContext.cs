68ca68d20d3124b63edfacb8b8cfa45a25c17830
hatef <hatef@HATEF-PC>
2020-11-29 12:43:07 +0330

    Add EFCore and Url Context

---
+++ ShortLinkGenerator.EF.Persistence/Contexts/URLContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShortLinkGenerator.EF.Persistence.Contexts
+{
+    public class URLContext : DbContext
+    {
+        public URLContext( DbContextOptions<URLContext> options ) : base(options)
+        {
+
+        }
+    }
+}
