316442d1daf2fc6b277b86ac926a23fb619ce8a8
hatef <hatef@HATEF-PC>
2020-11-29 15:09:32 +0330

    Add Url services

---
+++ ShortLinkGenerator.ApplicationContracts/Interfaces/Commands/IUrlCommandService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortLinkGenerator.ApplicationContracts.Interfaces.Commands
+{
+    public interface IUrlCommandService
+    {
+        Task<long> GenerateLink(string link);
+    }
+}
---
+++ ShortLinkGenerator.ApplicationContracts/Interfaces/Queries/IUrlQueryService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortLinkGenerator.ApplicationContracts.Interfaces.Queries
+{
+    public interface IUrlQueryService
+    {
+        Task<string> GetShortLink(long id);
+    }
+}
