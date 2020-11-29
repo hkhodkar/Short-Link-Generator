b842d7e8e3078818b398bf747608779dd22802ef
hatef <hatef@HATEF-PC>
2020-11-29 15:19:33 +0330

    Implement Url Services

---
+++ ShortLinkGenerator.ApplicationServices/Services/Commands/UrlCommandService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ShortLinkGenerator.DomainContracts.Interfaces.Commamds;
+using ShortLinkGenerator.ApplicationContracts.Interfaces.Commands;
+
+namespace ShortLinkGenerator.ApplicationServices.Services.Commands
+{
+    class UrlCommandService : IUrlCommandService
+    {
+        private readonly IUrlCommandRepository _urlCommandRepository;
+
+        public UrlCommandService(IUrlCommandRepository urlCommandRepository)
+        {
+            _urlCommandRepository = urlCommandRepository;
+        }
+        
+        public async Task<long> GenerateLink(string link)
+        {
+          return await  _urlCommandRepository.AddUrl(link);
+        }
+    }
+}
---
+++ ShortLinkGenerator.ApplicationServices/Services/Queries/UrlQueryService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ShortLinkGenerator.ApplicationContracts.Interfaces.Queries;
+using ShortLinkGenerator.DomainContracts.Interfaces.Queries;
+
+namespace ShortLinkGenerator.ApplicationServices.Services.Queries
+{
+    public class UrlQueryService : IUrlQueryService
+    {
+        private readonly IUrlQueryRepository _urlQueryRepository;
+
+        public UrlQueryService(IUrlQueryRepository urlQueryRepository)
+        {
+            _urlQueryRepository = urlQueryRepository;
+        }
+
+        public async Task<string> GetShortLink(long id)
+        {
+           return await _urlQueryRepository.GetShortLink(id);
+        }
+    }
+}
