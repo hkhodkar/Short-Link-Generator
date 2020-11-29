e0834c73a813a93ed0129d9509a11580bada5ed8
hatef <hatef@HATEF-PC>
2020-11-29 15:09:53 +0330

    Add UrlVisitorsCounter services

---
+++ ShortLinkGenerator.ApplicationContracts/Interfaces/Commands/IUrlVisitorsCounterCommandService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortLinkGenerator.ApplicationContracts.Interfaces.Commands
+{
+    public interface IUrlVisitorsCounterCommandService
+    {
+        Task AddCount(string linkCode);
+    }
+}
---
+++ ShortLinkGenerator.ApplicationContracts/Interfaces/Queries/IUrlVisitorsCounterQueryService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortLinkGenerator.ApplicationContracts.Interfaces.Queries
+{
+    public interface IUrlVisitorsCounterQueryService
+    {
+        Task<long> GetUrlVisitorsCount(string linkCode);
+    }
+}
