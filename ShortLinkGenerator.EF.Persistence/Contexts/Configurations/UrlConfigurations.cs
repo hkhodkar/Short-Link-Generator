e3eafbfc9aab1ede1378590973081ac79654e314
hatef <hatef@HATEF-PC>
2020-11-29 12:49:49 +0330

    Add url DbSet and url configurations

---
+++ ShortLinkGenerator.EF.Persistence/Contexts/Configurations/UrlConfigurations.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShortLinkGenerator.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShortLinkGenerator.EF.Persistence.Contexts.Configurations
+{
+    public class UrlConfigurations : IEntityTypeConfiguration<Url>
+    {
+        public void Configure(EntityTypeBuilder<Url> builder)
+        {
+            builder.ToTable("Url", "URL");
+            builder.HasKey(b => b.UrlId);
+            builder.Property(b => b.Link)
+                   .IsRequired()
+                   .HasMaxLength(2048);
+
+
+            builder.Property(b => b.CreatedDate)
+                   .HasColumnType("datetime")
+                   .HasDefaultValueSql("(getdate())");
+
+            builder.Property(b => b.ShortLink)
+                   .HasMaxLength(10)
+                   .IsRequired(true);
+        }
+    }
+}
--- ShortLinkGenerator.EF.Persistence/Contexts/URLContext.cs
+++ ShortLinkGenerator.EF.Persistence/Contexts/URLContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ShortLinkGenerator.Core.Entities;
+using ShortLinkGenerator.EF.Persistence.Contexts.Configurations;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,9 +9,19 @@ namespace ShortLinkGenerator.EF.Persistence.Contexts
 {
     public class URLContext : DbContext
     {
-        public URLContext( DbContextOptions<URLContext> options ) : base(options)
+        public URLContext(DbContextOptions<URLContext> options) : base(options)
+        {}
+
+
+        public DbSet<Url> Urls { get; set; }
+
+
+        #region OnModelCreating
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.ApplyConfiguration(new UrlConfigurations());
 
         }
+        #endregion
     }
 }
