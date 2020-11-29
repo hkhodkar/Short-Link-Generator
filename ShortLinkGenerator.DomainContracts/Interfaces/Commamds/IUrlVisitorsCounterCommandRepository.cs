c6067b0560a4a9df746f180a67be571a885a101b
hatef <hatef@HATEF-PC>
2020-11-29 13:13:09 +0330

    Add urlVisitorsCounter repositories

---
+++ ShortLinkGenerator.DomainContracts/Interfaces/Commamds/IUrlVisitorsCounterCommandRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortLinkGenerator.DomainContracts.Interfaces.Commamds
+{
+  public  interface IUrlVisitorsCounterCommandRepository
+    {
+        Task AddCounter(Guid linkCode);
+    }
+}
