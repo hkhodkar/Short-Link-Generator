186c642ab3404dbcd64b46095c23774806853e43
hatef <hatef@HATEF-PC>
2020-11-29 12:38:40 +0330

    Add UrlVisitorsCounter entity

---
+++ ShortLinkGenerator.Core/Entities/UrlVisitorsCounter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShortLinkGenerator.Core.Entities
+{
+  public  class UrlVisitorsCounter
+    {
+        public long CounterId { get; private set; }
+        public long Count { get; private set; }
+        public Guid LinkCode { get; private set; }
+        public DateTime LastVisited { get; private set; }
+
+        public UrlVisitorsCounter()
+        {
+
+        }
+
+        public UrlVisitorsCounter(Guid linkCode)
+        {
+            this.LinkCode = linkCode;
+        }
+    }
+}
