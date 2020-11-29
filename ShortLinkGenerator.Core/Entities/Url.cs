cfa0701ce5b387ad3f8cb709bce1542b115ce804
hatef <hatef@HATEF-PC>
2020-11-29 12:37:17 +0330

    Add Url entity

---
+++ ShortLinkGenerator.Core/Entities/Url.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace ShortLinkGenerator.Core.Entities
+{
+    public class Url
+    {
+        public long UrlId { get; private set; }
+        public string Link { get; private set; }
+        public Guid LinkCode { get; private set; }
+        public string ShortLink { get; private set; }
+        public DateTime CreatedDate { get; private set; }
+
+        public Url()
+        {
+
+        }
+
+        public Url(string link, string shortLink, Guid linkCode)
+        {
+            this.Link = link;
+            this.ShortLink = shortLink;
+            this.LinkCode = linkCode;
+        }
+    }
+}
