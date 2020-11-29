1e42941d60e6856af2ebf4d42457ab7dce47339b
hatef <hatef@HATEF-PC>
2020-11-29 13:12:29 +0330

    Add url repositories

---
+++ ShortLinkGenerator.DomainContracts/Interfaces/Commamds/IUrlCommandRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortLinkGenerator.DomainContracts.Interfaces.Commamds
+{
+  public  interface IUrlCommandRepository
+    {
+        Task<string> AddUrl(string Link);
+    }
+}
